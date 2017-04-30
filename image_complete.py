4bd160cf0782ef8d974d66eecd05876e3e0a95d8
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-04-29 21:41:25 -0400

    Update

--- image_complete.py
+++ image_complete.py
@@ -8,12 +8,16 @@ def load_destroyed_image_matrix(dir):
     pic = Image.open(dir)
     pic = pic.convert("L")
     pix = np.array(pic, float)
+    # f = open('destroyed_matrix', 'w')
+    # f.write(pix)
+    # f.close()
+    np.savetxt('original_destroyed_matrix.txt', pix.astype('uint8'), fmt='%i', delimiter=' ', newline='\n')
     new_pic = Image.fromarray(pix.astype('uint8'))
     new_pic.save("new_destroyed.png")
     for x in np.nditer(pix, op_flags=['readwrite']):
         if x[...] < 10.0:
             x[...] = np.nan
-
+    np.savetxt('destroyed_matrix.txt(with Nan)', pix, delimiter=' ', newline='\n')
     return pix
 
 
@@ -29,6 +33,9 @@ if __name__ == "__main__":
 
     data = load_destroyed_image_matrix(destroyed_img_dir)
     mat = complete_image(data)
-    print(mat)
+    # f = open('completed_matrix', 'w')
+    # f.write(mat)
+    # f.close()
+    np.savetxt('completed_matrix.txt', mat.astype('uint8'), fmt='%i', delimiter=' ', newline='\n')
     completed_pic = Image.fromarray(mat)
     completed_pic.save(restored_img_dir)
